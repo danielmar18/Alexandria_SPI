4b91abb36f2ab90ee66afb27b81dd353ef196d63
danielmar18 <33004159+danielmar18@users.noreply.github.com>
2020-07-27 18:17:34 +0000

    Fixing the Schema data

--- routes/schemaRoutes.js
+++ routes/schemaRoutes.js
@@ -11,33 +11,133 @@ server.post('/find', async (req, res) => {
     var collection = await DB.db.listCollections().toArray();
     var collections = [];
     collection.forEach(base => {
-        var object = {
-            "name": base.name,
-            "id": base.info.uuid
-        }
-        collections.push(object);
+
     });
-    console.log('\n');
-    console.log(req.body);
-    console.log('\n');
+
     return res.json(collections);
 })
 
 server.post('/list', async (req, res) => {
-    var collection = await DB.db.listCollections().toArray();
-    var collections = [];
-    collection.forEach(base => {
-        var object = {
-            "name": base.name,
-            "id": base.info.uuid
-        }
-        collections.push(object);
+    var schemaArray = [];
+    var counter = 0;
+    var collections = await DB.db.listCollections().toArray();
+    const numberOfCollections = collections.length;
+    collections.forEach(coll => {
+        var a = [];
+        DB.db.collection(coll.name.toString()).findOne({}, function(err, result){
+            if(err){console.log('We fucked up');}
+            else{
+                fieldArr = {};
+                var obj = JSON.stringify(result);
+                lines = obj.split(',');
+                lines.forEach( line => {
+                    var tmp = '';
+                    if(line[0] == '{'){
+                        tmp = line+'}';
+                    } else if(line[line.length-1] == '}'){
+                        tmp = '{'+line;
+                    } else{
+                        tmp = '{' + line + '}';
+                    }
+    
+                    tmpField = line.split(':')[0];
+                    tmpFieldType = typeFinder(line.split(/:(.+)/)[1]);
+                    tmpField2 = tmpField.split('"')[1];
+                    //var foo = {};
+                    //var name = tmpField2.toString();
+                    fieldObj = {    
+                        "displayName": tmpField2,
+                        "type": tmpFieldType,
+                        "queryOperators": [
+                            "eq",
+                            "lt",
+                            "gt",
+                            "hasSome",
+                            "and",
+                            "lte",
+                            "gte",
+                            "or",
+                            "not",
+                            "ne",
+                            "startsWith",
+                            "endsWith"
+                        ]
+                    };
+                    //foo[tmpField2.toString()] = fieldObj;
+                    fieldArr[tmpField2.toString()] = fieldObj;
+                });
+                schema = {
+                    "displayName": coll.name,
+                    "id": coll.name,
+                    "maxPageSize": 50,
+                    "allowedOperations": [
+                        "get",
+                        "find",
+                        "count"
+                    ],
+                    "ttl": 3600,
+                    "fields": fieldArr 
+                }
+                schemaArray.push(schema);
+            }
+            retObj = {
+                "schemas": schemaArray
+            }
+            counter++;
+            if(counter == numberOfCollections){
+                console.log(retObj);
+            }
+        });
     });
-    console.log('\n');
-   console.log(req.body);
-    console.log('\n');
-    return res.json(collections);
 })
 
+const typeFinder = line => {
+    if(line[line.length -1] == '}'){
+        line = line.substr(0, line.length -1);
+    }
+    if(Number(line).toString() != 'NaN'){
+        return 'number';
+    }
+    if(new Date(line.split('T')[0]) != 'Invalid Date'){
+        return 'datetime';
+    }
+    if(line === 'true' || line === 'false'){
+        return 'boolean';
+    }
+    else{
+        return 'text';
+    }
+}
+
+const fieldFinder = collectionName => {
+    DB.db.collection(collectionName).findOne({}, function(err, result){
+        if(err){console.log('We fucked up');}
+        else{
+            retArr = [];
+            var obj = JSON.stringify(result);
+            lines = obj.split(',');
+            lines.forEach( line => {
+                var tmp = '';
+                if(line[0] == '{'){
+                    tmp = line+'}';
+                } else if(line[line.length-1] == '}'){
+                    tmp = '{'+line;
+                } else{
+                    tmp = '{' + line + '}';
+                }
+
+                tmpField = line.split(':')[0];
+                tmpFieldType = typeFinder(line.split(/:(.+)/)[1]);
+                tmpField2 = tmpField.split('"')[1];
+                fieldObj = {
+                    "field": tmpField2,
+                    "fieldType": tmpFieldType
+                };
+                retArr.push(fieldObj);
+            });
+            return res.json(retArr);
+        }
+    });
+}
 
 module.exports = server;
