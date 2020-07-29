817ad339ca9b1ec88e3916174817bde7aa8597d8
danielmar18 <33004159+danielmar18@users.noreply.github.com>
2020-07-29 19:11:53 +0000

    Fixiing every little bug

--- services/estateService.js
+++ services/estateService.js
@@ -8,64 +8,25 @@ const estateService = () => {
         const collectionNameSchema = require(collectionNameSchemaString.toString());
         var collectionNameString = collectionName[0].toUpperCase()+collectionName.substr(1);
         var base = DB.model(collectionNameString, collectionNameSchema); 
-        base.find({}, function(err, result){
-            if(err){
-                errorCallback({status: 500, message: err});
-            } else{
-                retArr = [];
-                result.forEach(obj => {
-                    objArr = JSON.stringify(obj).split(',');
-                    fieldArr = [];
-                    valueArr = [];
-                    objArr.forEach( element => {
-                        fieldArr.push(element.split(':')[0]);
-                        valueArr.push(element.split(':')[1]);
-                    });
-                    fieldArr[0] = fieldArr[0].substr(1);
-                    valueArr[valueArr.length-1] = valueArr[valueArr.length-1].substr(0, valueArr[valueArr.length-1].length-1);
-                    var i = 0;
-                    var returnJson = {};
-                    valueArr.forEach( value => {
-                        if(typeFinder(value) === 'datetime'){
-                            var dateObj = {
-                                "$date": new Date(value.split('T')[0])
-                            }
-                            returnJson[fieldArr[i].toString().split('"')[1]] = dateObj;
-                        } else{
-                            returnJson[fieldArr[i].toString().split('"')[1]] = (Number(value).toString() != 'NaN') ? Number(value) : 
-                            (value == 'false' || value == 'true') ? ((value == 'true') ? true : false) : value.split('"')[1];
-                        }
-                        i++
+        if(body.filter == null || body.filter == 0){
+            base.find({}, function(err, result){
+                if(err){
+                    errorCallback({status: 500, message: err});
+                } else{
+                    var retArr = [];
+                    result.forEach(obj => {
+                        retArr.push(estatePopulator(obj))
                     })
-                    retArr.push(returnJson);
-                });
-                var jsonItems = (body.sort == null || body.sort == 0) ? retArr : retArr.sort(function(a, b){
-                    if(body.sort[0].direction == 'asc'){
-                        if(a[body.sort[0].fieldName] < b[body.sort[0].fieldName]){
-                            return -1;
-                        }else if(b[body.sort[0].fieldName] < a[body.sort[0].fieldName]){
-                            return 1;
-                        } else{
-                            return 0;
-                        }
-                    } else if(body.sort[0].direction == 'desc'){
-                        if(a[body.sort[0].fieldName] < b[body.sort[0].fieldName]){
-                            return 1;
-                        }else if(b[body.sort[0].fieldName] < a[body.sort[0].fieldName]){
-                            return -1;
-                        } else{
-                            return 0;
-                        }
+                    retObj = {
+                        items: sorter(body.sort, retArr),
+                        totalCount: retArr.length
                     }
-                })
-                retObj = {
-                    items: jsonItems,
-                    totalCount: retArr.length
+                    callback({status: 200, package: retObj});
                 }
-                console.log(retObj);
-                callback({status: 200, package: retObj});
-            }
-        })
+            });
+        } else{
+
+        }
     }
 
 
@@ -81,36 +42,13 @@ const estateService = () => {
                 if(estate == null){
                     errorCallback({status: 404, message: 'Estate not found'});
                 } else{
-                    objArr = JSON.stringify(estate).split(',');
-                    fieldArr = [];
-                    valueArr = [];
-                    objArr.forEach( element => {
-                        fieldArr.push(element.split(':')[0]);
-                        valueArr.push(element.split(':')[1]);
-                    });
-                    fieldArr[0] = fieldArr[0].substr(1);
-                    valueArr[valueArr.length-1] = valueArr[valueArr.length-1].substr(0, valueArr[valueArr.length-1].length-1);
-                    var i = 0;
-                    var returnJson = {};
-                    valueArr.forEach( value => {
-                        if(typeFinder(value) === 'datetime'){
-                            var dateObj = {
-                                "$date": new Date(value.split('T')[0])
-                            }
-                            returnJson[fieldArr[i].toString().split('"')[1]] = dateObj;
-                        } else{
-                            returnJson[fieldArr[i].toString().split('"')[1]] = (Number(value).toString() != 'NaN') ? Number(value) : 
-                            (value == 'false' || value == 'true') ? ((value == 'true') ? true : false) : value.split('"')[1];
-                        }
-                        i++
-                    })
                     retObj = {
-                        "item": returnJson
+                        "item": estatePopulator(estate)
                     }
                     callback({status: 200, package: retObj});
                 }
             }
-        })
+        });
     }
     return {
         findAllEstates,
@@ -118,6 +56,55 @@ const estateService = () => {
     }
 }
 
+const sorter = (sortOrder, dataArray) => {
+    var jsonItems = (sortOrder == null || sortOrder == 0) ? dataArray : dataArray.sort(function(a, b){
+        if(sortOrder[0].direction == 'asc'){
+            if(a[sortOrder[0].fieldName] < b[sortOrder[0].fieldName]){
+                return -1;
+            }else if(b[sortOrder[0].fieldName] < a[sortOrder[0].fieldName]){
+                return 1;
+            } else{
+                return 0;
+            }
+        } else if(sortOrder[0].direction == 'desc'){
+            if(a[sortOrder[0].fieldName] < b[sortOrder[0].fieldName]){
+                return 1;
+            }else if(b[sortOrder[0].fieldName] < a[sortOrder[0].fieldName]){
+                return -1;
+            } else{
+                return 0;
+            }
+        }
+    });
+    return jsonItems;
+}
+const estatePopulator = (obj) => {
+    objArr = JSON.stringify(obj).split(',');
+    fieldArr = [];
+    valueArr = [];
+    objArr.forEach( element => {
+        fieldArr.push(element.split(':')[0]);
+        valueArr.push(element.split(':')[1]);
+    });
+    fieldArr[0] = fieldArr[0].substr(1);
+    valueArr[valueArr.length-1] = valueArr[valueArr.length-1].substr(0, valueArr[valueArr.length-1].length-1);
+    var i = 0;
+    var returnJson = {};
+    valueArr.forEach( value => {
+        if(typeFinder(value) === 'datetime'){
+            var dateObj = {
+                "$date": new Date(value.split('T')[0])
+            }
+            returnJson[fieldArr[i].toString().split('"')[1]] = dateObj;
+        } else{
+            returnJson[fieldArr[i].toString().split('"')[1]] = (Number(value).toString() != 'NaN') ? Number(value) : 
+            (value == 'false' || value == 'true') ? ((value == 'true') ? true : false) : value.split('"')[1];
+        }
+        i++
+    });
+    return returnJson;
+
+}
 const typeFinder = line => {
     if(line[line.length -1] == '}'){
         line = line.substr(0, line.length -1);
